d5a4bd92b1c3c43e7dd1cdbedf50d18f87d4eecf
Sabirjan <253933559@qq.com>
2014-12-17 15:30:04 +0800

    删除了
    删除了

--- UyghurSoftKeyBoardDemo/src/com/izqisoft/keyboard/Global.java
+++
@@ -1,9 +0,0 @@
-package com.izqisoft.keyboard;
-
-import android.graphics.Typeface;
-
-public class Global {
-
-	public static Typeface typeface;
-	public static UyghurCharUtilities UyghurConvert=UyghurCharUtilities.getUtilities();
-}
